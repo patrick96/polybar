339dfaa07b1b3360629edb400264a666eb4f8aa7
patrick96 <p.ziegler96@gmail.com>
2020-06-01 22:56:50 +0200

    Test7

--- doc/conf.py
+++ doc/conf.py
@@ -62,6 +62,9 @@ extensions = [
 
 
 if on_rtd:
+  versionwarning_messages = {
+      'latest': 'You are reading the documentation for the latest development version of polybar. The content may not reflect the state of the newest stable release. {newest} is the newest stable release.'
+  }
   extensions.append('versionwarning.extension')
 
 
